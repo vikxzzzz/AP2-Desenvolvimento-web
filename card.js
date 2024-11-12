158dbc5b73011e98a53009ec3fa631f64c512eef
Viktor <viktormb2005@gmail.com>
2024-11-12 15:38:57 -0300

    a

---
+++ card.js
@@ -0,0 +1,35 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const atletaDetalhes = document.getElementById("atletaDetalhes");
+    
+    const params = new URLSearchParams(window.location.search);
+    const id = params.get("id");
+
+    const urlDetalhes = `https://botafogo-atletas.mange.li/2024-1/${id}`;
+
+    const pega_json = async (caminho) => {
+        const resposta = await fetch(caminho);
+        const dados = await resposta.json();
+        return dados;
+    };
+
+    const exibeDetalhes = (atleta) => {
+        const nome = document.createElement("h2");
+        const imagem = document.createElement("img");
+        const descricao = document.createElement("p");
+        const altura = document.createElement("p");
+
+        nome.innerHTML = atleta.nome;
+        imagem.src = atleta.imagem;
+        descricao.innerHTML = atleta.detalhes;
+        altura.innerHTML = `Altura: ${atleta.altura} m`;
+
+        atletaDetalhes.appendChild(nome);
+        atletaDetalhes.appendChild(imagem);
+        atletaDetalhes.appendChild(descricao);
+        atletaDetalhes.appendChild(altura);
+    };
+
+    pega_json(urlDetalhes).then((atleta) => {
+        exibeDetalhes(atleta);
+    });
+});
--- detalhes.js
+++ detalhes.js
@@ -1,75 +1,85 @@
-const urlAll = "https://botafogo-atletas.mange.li/2024-1/all";
-const urlMasc = "https://botafogo-atletas.mange.li/2024-1/masculino";
-const urlFem = "https://botafogo-atletas.mange.li/2024-1/feminino";
+document.addEventListener("DOMContentLoaded", () => {
+    const urlAll = "https://botafogo-atletas.mange.li/2024-1/all";
+    const urlMasc = "https://botafogo-atletas.mange.li/2024-1/masculino";
+    const urlFem = "https://botafogo-atletas.mange.li/2024-1/feminino";
 
-const container = document.getElementById("container");
+    const container = document.getElementById("container");
 
-const pega_json = async (caminho) => {
-    const resposta = await fetch(caminho);
-    const dados = await resposta.json();
-    return dados;
-};
-
-const montaCard = (atleta) => {
-    const cartao = document.createElement("article");
-    const nome = document.createElement("h1");
-    const imagem = document.createElement("img");
-    const descri = document.createElement("p");
-
-    nome.innerHTML = atleta.nome;
-    nome.style.fontFamily = "sans-serif";
-    cartao.appendChild(nome);
-
-    imagem.src = atleta.imagem;
-    cartao.appendChild(imagem);
-
-    descri.innerHTML = atleta.detalhes;
-    cartao.appendChild(descri);
-
-    cartao.dataset.id = atleta.id;
-    cartao.dataset.altura = atleta.altura;
-
-    return cartao;
-};
-
-const acessarCard = () => {
-    const all = document.getElementById('botaoall');
-    const fem = document.getElementById('botaofem');
-    const masc = document.getElementById('botaomasc');
-
-
-    all.onclick = () => {
-        container.innerHTML = ''; 
-        pega_json(`${urlAll}`).then(
-            (r) => {
-                r.forEach(
-                    (ele) => container.appendChild(montaCard(ele))
-                );
-            }
-        )
+    const pega_json = async (caminho) => {
+        const resposta = await fetch(caminho);
+        const dados = await resposta.json();
+        return dados;
     };
 
-    fem.onclick = () => {
-        container.innerHTML = ''; 
-        pega_json(`${urlFem}`).then(
-            (r) => {
-                r.forEach(
-                    (ele) => container.appendChild(montaCard(ele))
-                );
-            }
-        )
+    const montaCard = (atleta) => {
+        const cartao = document.createElement("article");
+        const nome = document.createElement("h1");
+        const imagem = document.createElement("img");
+        const saibaMais = document.createElement("p");
+
+        nome.innerHTML = atleta.nome;
+        nome.style.fontFamily = "sans-serif";
+        cartao.appendChild(nome);
+
+        imagem.src = atleta.imagem;
+        cartao.appendChild(imagem);
+
+        saibaMais.innerHTML = "Clique no card para saber mais";
+        cartao.appendChild(saibaMais);
+
+        cartao.dataset.id = atleta.id;
+
+        cartao.onclick = () => {
+            window.location.href = `card.html?id=${atleta.id}`;
+        };
+
+        return cartao;
     };
 
-    masc.onclick = () => {
-        container.innerHTML = ''; 
-        pega_json(`${urlMasc}`).then(
-            (r) => {
-                r.forEach(
-                    (ele) => container.appendChild(montaCard(ele))
-                );
-            }
-        )
-    };
-};
+    const acessarCard = () => {
+        const all = document.getElementById('botaoall');
+        const fem = document.getElementById('botaofem');
+        const masc = document.getElementById('botaomasc');
 
-acessarCard();
+        if (all) {
+            all.onclick = () => {
+                container.innerHTML = ''; 
+                pega_json(`${urlAll}`).then(
+                    (r) => {
+                        r.forEach(
+                            (ele) => container.appendChild(montaCard(ele))
+                        );
+                    }
+                );
+            };
+        }
+
+        if (fem) {
+            fem.onclick = () => {
+                container.innerHTML = ''; 
+                pega_json(`${urlFem}`).then(
+                    (r) => {
+                        r.forEach(
+                            (ele) => container.appendChild(montaCard(ele))
+                        );
+                    }
+                );
+            };
+        }
+
+        if (masc) {
+            masc.onclick = () => {
+                container.innerHTML = ''; 
+                pega_json(`${urlMasc}`).then(
+                    (r) => {
+                        r.forEach(
+                            (ele) => container.appendChild(montaCard(ele))
+                        );
+                    }
+                );
+            };
+        }
+    };
+
+    acessarCard();
+});
