05e7007b52e25fd895d0416dae504b2544e3bc0e
Viktor <viktormb2005@gmail.com>
2024-11-18 20:39:20 -0300

    atualização

--- detalhes.js
+++ detalhes.js
@@ -1,9 +1,9 @@
-
 const urlAll = "https://botafogo-atletas.mange.li/2024-1/all";
 const urlMasc = "https://botafogo-atletas.mange.li/2024-1/masculino";
 const urlFem = "https://botafogo-atletas.mange.li/2024-1/feminino";
 
 const container = document.getElementById("container");
+const pesquisaInput = document.getElementById('filtrar-jogadores');
 
 const pega_json = async (caminho) => {
     const resposta = await fetch(caminho);
@@ -37,41 +37,66 @@ const montaCard = (atleta) => {
 };
 
 
-
 const botaoLogout = () => {
-    const logout = document.getElementById("logout");
-    logout.onclick = () => {
-        sessionStorage.removeItem("logado");
-        window.location.href = "index.html";
-    };
-}
+    const logout = document.getElementById("logout-detalhes");
+    if (logout) {
+        logout.onclick = () => {
+            sessionStorage.removeItem("logado");
+            window.location.href = "index.html";
+        };
+    }
+};
+
+const barraPesquisa = (jogadores) => {
+    pesquisaInput.addEventListener("input", (e) => {
+        const valorInput = e.target.value.toLowerCase();
+        const atletasFiltrados = jogadores.filter((atleta) =>
+            atleta.nome.toLowerCase().includes(valorInput)
+        );
+        container.innerHTML = '';
+        atletasFiltrados.forEach((ele) => container.appendChild(montaCard(ele)));
+    });
+};
+
 
 const acessarCard = () => {
     botaoLogout();
+
     const all = document.getElementById('botaoall');
     const fem = document.getElementById('botaofem');
     const masc = document.getElementById('botaomasc');
-    
-    all.onclick = () => {
-        container.innerHTML = '';
-        pega_json(`${urlAll}`).then((r) => {
-            r.forEach((ele) => container.appendChild(montaCard(ele)));
-        });
-    };
 
-    fem.onclick = () => {
-        container.innerHTML = '';
-        pega_json(`${urlFem}`).then((r) => {
-            r.forEach((ele) => container.appendChild(montaCard(ele)));
-        });
-    };
+    if (all) {
+        all.onclick = () => {
+            container.innerHTML = '';
+            pega_json(urlAll).then((jogadores) => {
+                jogadores.forEach((ele) => container.appendChild(montaCard(ele)));
+                barraPesquisa(jogadores); 
+            });
+        };
+    }
 
-    masc.onclick = () => {
-        container.innerHTML = '';
-        pega_json(`${urlMasc}`).then((r) => {
-            r.forEach((ele) => container.appendChild(montaCard(ele)));
-        });
-    };
+    if (fem) {
+        fem.onclick = () => {
+            container.innerHTML = '';
+            pega_json(urlFem).then((jogadores) => {
+                jogadores.forEach((ele) => container.appendChild(montaCard(ele)));
+                barraPesquisa(jogadores); 
+            });
+        };
+    }
+
+    if (masc) {
+        masc.onclick = () => {
+            container.innerHTML = '';
+            pega_json(urlMasc).then((jogadores) => {
+                jogadores.forEach((ele) => container.appendChild(montaCard(ele)));
+                barraPesquisa(jogadores);
+            });
+        };
+    }
 };
 
-acessarCard();
+document.addEventListener("DOMContentLoaded", () => {
+    acessarCard();
+});
